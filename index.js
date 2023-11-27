39b94f4348f4f16f15f0fac7ac7712e2b141cdf1
Lkhamjav-github <lkhamjavsanjmytav@gmail.com>
2023-11-27 12:57:06 +0800

    haha

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+
+
 const addBtn = document.getElementById("addBtn");
 addBtn.addEventListener('click',function(){
-    
+    const modal = document.getElementById("modal");
+    modal.style.opacity = "1";
 })
