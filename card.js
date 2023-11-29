0eb3c245a2043ad3842c2bbb5cd50af8915ac5d4
Lhamjav Sanjmyatav <23LP0694@G302-CC0067.local>
2023-11-29 19:51:46 +0800

    svvlde tolgoi owdcloo

--- card.js
+++ card.js
@@ -1,74 +1,74 @@
 
-function render(){
+export function render() {
 
-const board = document.getElementById("board"); //boards duudaj bn 
-const middle = document.getElementById("middle");
-const boardInProgress = document.getElementById("board-in-progress");
-const boardStuck = document.getElementById("board-stuck");
-const boardDone = document.getElementById("board-done");
-const card = document.createElement("div"); //card create hiij bga 
-
-
-//boardiin huuuhed bolgsnoor html deer nemgdej hargdann 
-
-boardInProgress.appendChild(card);
-boardStuck.appendChild(card);
-boardDone.appendChild(card);
-const correct = document.getElementById("fa-circle-check"); //correct temdeg oruulahin tuld class neej bn 
-
-card.appendChild(correct); //cardiinha hvvhed bolgoj bn
+    const board = document.getElementById("board"); //boards duudaj bn 
+    const middle = document.getElementById("middle");
+    const boardInProgress = document.getElementById("board-in-progress");
+    const boardStuck = document.getElementById("board-stuck");
+    const boardDone = document.getElementById("board-done");
+    const card = document.createElement("div"); //card create hiij bga 
 
 
-const details = document.createElement("div"); //details dund zuils hiih div
+    //boardiin huuuhed bolgsnoor html deer nemgdej hargdann 
 
-card.appendChild(details); //huuhed n bolgson odoo  dotroh elmentvvdiig 
+    boardInProgress.appendChild(card);
+    boardStuck.appendChild(card);
+    boardDone.appendChild(card);
+    const correct = document.getElementById("fa-circle-check"); //correct temdeg oruulahin tuld class neej bn 
+
+    card.appendChild(correct); //cardiinha hvvhed bolgoj bn
 
 
-const h4 = document.createElement("h4");
-const p = document.createElement("p");
-const levelBtn = document.createElement("button");
-h4.innerText = "Todo";
-p.innerText = "This is a todo card";
-const arr = ['low', 'medium', 'high'];
-const low = document.getElementById("low")
-const medium = document.getElementById("medium")
-const high = document.getElementById("high")
-levelBtn.innerText = arr[0];
+    const details = document.createElement("div"); //details dund zuils hiih div
 
-details.appendChild(h4);
-details.appendChild(p);
-details.appendChild(levelBtn);
-const actions = document.createElement("div");
+    card.appendChild(details); //huuhed n bolgson odoo  dotroh elmentvvdiig 
 
-const xmark = document.getElementById("fa-circle-xmark");
-const pen = document.getElementById("fa-pen-to-square");
-actions.appendChild(xmark);
-actions.appendChild(pen);
-middle.appendChild(card); 
-xmark.style.fontSize = "1.25em";
-pen.style.fontSize = "1.25em";
-card.appendChild(actions);
-actions.style.display = "flex";
-actions.style.flexDirection = "column";
-actions.style.gap = "8px";
 
-levelBtn.style.width = "fit-content"
-levelBtn.style.padding = "4px 8px"
+    const h4 = document.createElement("h4");
+    const p = document.createElement("p");
+    const levelBtn = document.createElement("button");
+    h4.innerText = "Todo";
+    p.innerText = "This is a todo card";
+    const arr = ['low', 'medium', 'high'];
+    const low = document.getElementById("low")
+    const medium = document.getElementById("medium")
+    const high = document.getElementById("high")
+    levelBtn.innerText = arr[0];
 
-details.style.flex = "1";
-details.style.flexBasis = "0";
-details.style.display = "flex";
-details.style.flexDirection = "column";
-details.style.gap = "12px";
+    details.appendChild(h4);
+    details.appendChild(p);
+    details.appendChild(levelBtn);
+    const actions = document.createElement("div");
 
-correct.style.fontSize = "1.25em";
+    const xmark = document.getElementById("fa-circle-xmark");
+    const pen = document.getElementById("fa-pen-to-square");
+    actions.appendChild(xmark);
+    actions.appendChild(pen);
+    middle.appendChild(card);
+    xmark.style.fontSize = "1.25em";
+    pen.style.fontSize = "1.25em";
+    card.appendChild(actions);
+    actions.style.display = "flex";
+    actions.style.flexDirection = "column";
+    actions.style.gap = "8px";
 
-card.style.padding = "8px";
-card.style.borderRadius = "4px";
-card.style.backgroundColor = "white";
-card.style.cursor = "pointer";
-card.style.display = "flex";
-card.style.gap = "12px";
+    levelBtn.style.width = "fit-content"
+    levelBtn.style.padding = "4px 8px"
+
+    details.style.flex = "1";
+    details.style.flexBasis = "0";
+    details.style.display = "flex";
+    details.style.flexDirection = "column";
+    details.style.gap = "12px";
+
+    correct.style.fontSize = "1.25em";
+
+    card.style.padding = "8px";
+    card.style.borderRadius = "4px";
+    card.style.backgroundColor = "white";
+    card.style.cursor = "pointer";
+    card.style.display = "flex";
+    card.style.gap = "12px";
 
 }
 // render();
--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const addTaskBtn = document.getElementById("addTask");
+const addingBtn = document.querySelectorAll(".adding-btn");
+const title = document.getElementById("title");
+const description = document.getElementById("description")
+const middleTodo = document.getElementById("middle-todo");
+const middleIn = document.getElementById("middle-inprogress");
+const middleStuck = document.getElementById("middle-stuck");
+const middleDone = document.getElementById("middle-done");
+const priority = document.getElementById("priority")
+
 
 function addTask() {
-    const middleTodo = document.getElementById("middle-todo");
-    const middleIn = document.getElementById("middle-inprogress");
-    const middleStuck = document.getElementById("middle-stuck");
-    const middleDone = document.getElementById("middle-done");
-    const title = document.getElementById("title");
-    const description = document.getElementById("description")
+    function removeTask(event) {
+        console.log(event.target.id)
+        const task = document.getElementById("task-" + event.target.id);
+        console.log(task)
+        task.remove()
+    }
 
-    const task = () => {
-        return `<div class="task"">
+    const taskComponent = (id) => {
+        return `<div id="task-${id}" class="task"" draggable="true">
         <i id="fa-circle-check" class="fa-regular fa-circle-check"></i>
         <div class="details">
             <h4>${title.value}</h4>
@@ -17,33 +26,56 @@ function addTask() {
             <button>${document.getElementById("priority").value}</button>
         </div>
         <div class="action">
-            <i id="fa-circle-xmark" class="fa-regular fa-circle-xmark" "></i>
+            <i id=${id} class="fa-regular fa-circle-xmark" "></i>
             <i id="fa-pen-to-square" class="fa-solid fa-pen-to-square" "></i>
         </div>
     </div>`
-
     }
+
     const tasks = document.getElementById("status").value
+    const id = "id" + Math.random().toString(16).slice(2)
     if (tasks === "todo") {
-        middleTodo.innerHTML += task();
+        middleTodo.innerHTML += taskComponent(id);
+        const modal = document.getElementById("modal");
+        modal.style.display = "none";
     }
     if (tasks === "inprogress") {
-        middleIn.innerHTML += task();
+        middleIn.innerHTML += taskComponent(id);
+        const modal = document.getElementById("modal");
+        modal.style.display = "none";
     }
     if (tasks === "stuck") {
-        middleStuck.innerHTML += task();
+        middleStuck.innerHTML += taskComponent(id);
+        const modal = document.getElementById("modal");
+        modal.style.display = "none";
     }
     if (tasks === "done") {
-        middleDone.innerHTML += task();
+        middleDone.innerHTML += taskComponent(id);
+        const modal = document.getElementById("modal");
+        modal.style.display = "none";
     }
+    else {
+        const modal = document.getElementById("modal");
+        modal.style.display = "none";
+    }
+    const removeBtn = document.getElementById(id);
+    console.log(removeBtn)
+
+
+    console.log(middleTodo)
 }
 
-// addTask()
+addTaskBtn.addEventListener("click", addTask);
 
-const addBtn = document.getElementById("addBtn");
-addBtn.addEventListener('click', function () {
-    const modal = document.getElementById("modal");
-    modal.style.display = "flex";
+const modal = document.getElementById("modal");
+addingBtn.forEach(addingBtn => {
+    addingBtn.onclick = () => {
+
+        modal.style.display = "flex";
+        const tasks = document.getElementById("status")
+        title.value = "";
+        description.value = "";
+        tasks.value = "";
+        priority.value = "";
+    }
 })
-
-addTaskBtn.addEventListener("click", addTask)
