78c5ef22d3c98471be7169fa5d236597717b814d
Lhamjav Sanjmyatav <23LP0694@G302-CC0067.local>
2023-12-01 19:37:34 +0800

    drag and drop uldsen

--- todo/script.js
+++ todo/script.js
@@ -10,6 +10,10 @@ const priority = document.getElementById("priority");
 const actions = document.createElement("div");
 const editTask = document.getElementById("editTask")
 actions.classList.add('actions');
+const todoCount = document.getElementById("todo-count");
+const todoIn = document.getElementById("in-count");
+const todoStuck = document.getElementById("stuck-count");
+const todoDone = document.getElementById("done-count");
 
 function addBtn() {
 
@@ -24,6 +28,7 @@ function addBtn() {
     pen.innerHTML = ` <i id="fa-pen-to-square" class="fa-solid fa-pen-to-square"></i>`
     const correct = document.createElement("div");
     correct.innerHTML = `<i id="fa-circle-check" class="fa-regular fa-circle-check"></i>`
+    correct.classList.add('correct');
 
     card.appendChild(correct);
     const details = document.createElement("div");
@@ -36,7 +41,7 @@ function addBtn() {
     const levelBtn = document.createElement("button");
     h4.innerText = title.value;
     p.innerText = description.value;
-    levelBtn.innerText = priority.value
+    levelBtn.innerText = priority.value;
 
     details.appendChild(h4);
     details.appendChild(p);
@@ -46,6 +51,16 @@ function addBtn() {
     actions.appendChild(pen);
     card.appendChild(actions)
 
+    correct.addEventListener("click", function () {
+        correct.style.backgroundColor = "black"
+        correct.style.color = "white"
+        const card = xmark.closest('.card');
+        if (card) {
+            middleDone.appendChild(card);
+        }
+        count()
+    })
+
 
     const tasks = document.getElementById("status").value
 
@@ -53,6 +68,7 @@ function addBtn() {
         middleTodo.appendChild(card)
         const modal = document.getElementById("modal");
         modal.style.display = "none";
+
     }
     if (tasks === "inprogress") {
         middleIn.appendChild(card)
@@ -68,11 +84,14 @@ function addBtn() {
         middleDone.appendChild(card)
         const modal = document.getElementById("modal");
         modal.style.display = "none";
+        correct.style.backgroundColor = "black"
+        correct.style.color = "white"
     }
     else {
         const modal = document.getElementById("modal");
         modal.style.display = "none";
     }
+
     xmark.addEventListener("click", function () {
 
         const card = xmark.closest('.card');
@@ -84,7 +103,6 @@ function addBtn() {
     pen.addEventListener("click", function () {
 
         title.value = h4.textContent
-
         modal.style.display = "flex"
         const card = xmark.closest('.card');
         if (card) {
@@ -92,9 +110,17 @@ function addBtn() {
         }
 
     })
+    count()
+
 }
+count()
+function count() {
 
-
+    todoCount.innerText = middleTodo.childElementCount;
+    todoDone.innerText = middleDone.childElementCount;
+    todoIn.innerText = middleIn.childElementCount;
+    todoStuck.innerText = middleStuck.childElementCount;
+}
 addTaskBtn.addEventListener("click", addBtn);
 
 const modal = document.getElementById("modal");
